fbda4d7a9b2dc85835684b3227dc57827a214fcb
ChenXinyong <463062191@qq.com>
2022-03-29 17:26:12 +0800

    逻辑代码

---
+++ MonsterGame/MonsterGame/Assets/Script/GameScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameScene : MonoBehaviour
+{
+    private GameObject btn_Return;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn_Return = GameObject.Find("btn_Return");
+        btn_Return.GetComponent<Button>().onClick.AddListener(delegate { SceneJump("MainScene"); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SceneJump(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ MonsterGame/MonsterGame/Assets/Script/MainScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainScene : MonoBehaviour
+{
+    private GameObject btn_GameStart, btn_Blood, btn_Acupoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn_GameStart = GameObject.Find("btn_GameStart");
+        btn_GameStart.GetComponent<Button>().onClick.AddListener(delegate { SceneJump("GameScene"); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SceneJump(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+
+}
