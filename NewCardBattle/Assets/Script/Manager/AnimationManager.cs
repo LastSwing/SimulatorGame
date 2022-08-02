a88472070894e4df56fc14bfefaebd31ea34f353
blue <335224756@qq.com>
2022-08-02 15:11:11 +0800

    动作特效规范

---
+++ NewCardBattle/Assets/Script/Manager/AnimationManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : SingletonMonoBehaviour<AnimationManager>
+{
+    /// <summary>
+    /// 动作/特效名，持续时间
+    /// </summary>
+    Dictionary<string, float> animationDic = new Dictionary<string, float>()
+    {
+        { "RoundChange",1.1f},
+        { "CardMove",1.2f},
+    };
+
+    /// <summary>
+    /// 执行动作/特效
+    /// </summary>
+    /// <param name="name">动作/特效名</param>
+    /// <param name="arg">参数集</param>
+    /// <returns>执行时间</returns>
+    public float DoAnimation(string name,object[] arg)
+    {
+        if (animationDic.ContainsKey(name))
+        {
+            switch (name)
+            {
+                case "RoundChange":
+                    break;
+                case "CardMove":
+                    break;
+            }
+            return animationDic[name];
+        }
+        return 0;
+    }
+}
